d8d5d94a6f563a7f6a9d2cdc4c6ba2ea3d733e78
yichen <yichen@yichendeMac.local>
2016-04-18 23:40:04 +0800

    upload git test.

---
+++ uploadTestDemo/uploadTestDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  uploadTestDemo
+//
+//  Created by yichen on 16-4-18.
+//  Copyright (c) 2016年 yichen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"the uploadGitDemo");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
